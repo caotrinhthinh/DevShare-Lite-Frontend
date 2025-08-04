545a00758bcc018e474c98e7884ba0966f5bae19
caotrinhthinh <caotrinhthinh@gmail.com>
2025-08-04 10:14:36 +0700

    Add footer component

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,6 +1,7 @@
 import Header from "./Header";
 import { ToastContainer } from "react-toastify";
 import { Outlet } from "react-router-dom";
+import Footer from "./Footer";
 
 const Layout = () => {
   return (
@@ -9,6 +10,7 @@ const Layout = () => {
       <main className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-8">
         <Outlet />
       </main>
+      <Footer />
       <ToastContainer
         position="top-right"
         autoClose={5000}
